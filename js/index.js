0bc0965ef74b6aaad0e49b8831e81fcb1fcbc27c
Antonio Tsai <jimmyjack7777@gmail.com>
2017-05-02 10:25:02 +0800

    Load data from resource.csv to fill the table

--- js/index.js
+++ js/index.js
@@ -5,5 +5,16 @@ var overview = new Vue({
       [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
       [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
     ],
+  },
+  created: function () {
+    fetch('/resource.csv').then(response => {
+      return response.text();
+    }).then(text => {
+      // Split text in to array
+      rows = text.split("\n");
+      this.rows = rows.map(row => {
+        return row.split(",");
+      });
+    });
   }
 });
