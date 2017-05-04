afd9912ee04603adf5175fc0b40bc6edbe1fcb45
Antonio Tsai <jimmyjack7777@gmail.com>
2017-05-04 12:38:07 +0800

    Remove title of the table from resource.csv

--- js/index.js
+++ js/index.js
@@ -1,10 +1,7 @@
 var overview = new Vue({
   el: '#overview',
   data: {
-    rows: [
-      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-      [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-    ],
+    rows: [],
   },
   created: function () {
     fetch('/resource.csv').then(response => {
@@ -12,6 +9,10 @@ var overview = new Vue({
     }).then(text => {
       // Split text in to array
       rows = text.split("\n");
+
+      // Cut off first two rows from csv
+      rows = rows.slice(2);
+
       this.rows = rows.map(row => {
         return row.split(",");
       });
