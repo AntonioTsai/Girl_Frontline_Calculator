da79349a4bd34e78424754b12205eb637c1e3b68
Antonio Tsai <jimmyjack7777@gmail.com>
2017-12-20 14:49:01 +0800

    Add Filter and links on homepage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <div>
+      <router-link to="/">Home</router-link>
+      <router-link to="/filter">Filter</router-link>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -13,7 +17,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Filter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="filter">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
+
