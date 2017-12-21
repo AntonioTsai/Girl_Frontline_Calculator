66d3a1822ab7fc25536150bfd14364f05abb7804
Antonio Tsai <jimmyjack7777@gmail.com>
2017-12-21 10:37:50 +0800

    Add resource filter using group checkbox

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -9,24 +9,62 @@
         <label>後勤隊伍</label>
         <el-input-number v-model="teamNumber" :min="1" :max="4"></el-input-number>
       </el-col>
+      <el-col :span="12">
+        <label class="vertical-center">資源</label>
+        <div class="inline-div">
+          <el-checkbox-group v-model="resourceFilter">
+            <el-checkbox-button v-for="res in resources" :label="res.label" :key="res.label">{{ res.title }}</el-checkbox-button>
+          </el-checkbox-group>
+        </div>
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-export default {
+const resources = [
+  {
+    label: 'human_resource',
+    title: '人力',
+  },
+  {
+    label: 'ration',
+    title: '彈藥',
+  },
+  {
+    label: 'ammo',
+    title: '口糧',
+  },
+  {
+    label: 'component',
+    title: '零件',
+  },
+];
 
 export default {
   data() {
     return {
       captainLevel: 1,
       teamNumber: 1,
+      resources,
+      resourceFilter: [
+        'human_resource',
+        'ammo',
+        'ration',
+        'component',
+      ],
     };
   },
 };
 </script>
 
 <style>
+label.vertical-center {
+  vertical-align: -webkit-baseline-middle;
+}
 
+.inline-div {
+  display: inline-block;
+}
 </style>
 
