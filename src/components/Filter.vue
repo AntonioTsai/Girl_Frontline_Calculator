1fc79959b6f9bbb5779b3f0b3ec4d440c9d27ea4
Antonio Tsai <jimmyjack7777@gmail.com>
2017-12-21 10:36:28 +0800

    Add input captainLevel and teamNumber

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="filter">
-
+    <el-row>
+      <el-col :span="6">
+        <label>隊長等級</label>
+        <el-input-number v-model="captainLevel" :min="1" :max="100"></el-input-number>
+      </el-col>
+      <el-col :span="6">
+        <label>後勤隊伍</label>
+        <el-input-number v-model="teamNumber" :min="1" :max="4"></el-input-number>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
 
+export default {
+  data() {
+    return {
+      captainLevel: 1,
+      teamNumber: 1,
+    };
+  },
 };
 </script>
 
