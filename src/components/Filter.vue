b81092057c3f5db33e7f74a02aec47e6c1d2f98e
Antonio Tsai <jimmyjack7777@gmail.com>
2017-12-29 17:42:58 +0800

    Update changes to store

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -3,16 +3,16 @@
     <el-row type="flex">
       <el-col :span="6">
         <label>隊長等級</label>
-        <el-input-number v-model="captainLevel" :min="1" :max="100"></el-input-number>
+        <el-input-number v-model="captainLevel" @change="handleCaptainLevel" :min="1" :max="100"></el-input-number>
       </el-col>
       <el-col :span="6">
         <label>後勤隊伍</label>
-        <el-input-number v-model="teamNumber" :min="1" :max="4"></el-input-number>
+        <el-input-number v-model="teamNumber" @change="handleTeamNumber" :min="1" :max="4"></el-input-number>
       </el-col>
       <el-col :span="12">
         <label class="vertical-center">資源</label>
         <div class="inline-div">
-          <el-checkbox-group v-model="resourceFilter">
+          <el-checkbox-group v-model="resourceFilter" @change="handleResourceFilter">
             <el-checkbox-button v-for="res in resources" :label="res.label" :key="res.label">{{ res.title }}</el-checkbox-button>
           </el-checkbox-group>
         </div>
@@ -54,6 +54,32 @@ export default {
         'component',
       ],
     };
+  },
+  methods: {
+    handleCaptainLevel(val) {
+      // eslint-disable-next-line
+      console.log(`Checkbox Group Changed!\nval = ${val}`);
+      this.$store.commit({
+        type: 'updateCaptainLevel',
+        level: val,
+      });
+    },
+    handleTeamNumber(val) {
+      // eslint-disable-next-line
+      console.log(`Checkbox Group Changed!\nval = ${val}`);
+      this.$store.commit({
+        type: 'updateTeamNumber',
+        number: val,
+      });
+    },
+    handleResourceFilter(val) {
+      // eslint-disable-next-line
+      console.log(`Checkbox Group Changed!\nval = ${val}`);
+      this.$store.commit({
+        type: 'updateResourceFilter',
+        filter: val,
+      });
+    },
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,24 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    captainLevel: 1,
+    teamNumber: 1,
+    resourceFilter: [
+      'human_resource',
+      'ammo',
+      'ration',
+      'component',
+    ],
+  },
+  mutations: {
+    updateCaptainLevel(state, payload) {
+      state.captainLevel = payload.level;
+    },
+    updateTeamNumber(state, payload) {
+      state.teamNumber = payload.number;
+    },
+    updateResourceFilter(state, payload) {
+      state.resourceFilter = payload.filter;
+    },
   },
 });
